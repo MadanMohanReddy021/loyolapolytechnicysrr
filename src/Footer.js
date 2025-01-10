6c3036252a1facbc340179fa742d6396c0b184a4
MadanMohanReddy021 <madanmohanreddy535@gmail.com>
2025-01-10 09:36:33 +0530

    CHANGED THE FOOTER

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,9 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-light text-black text-center py-4 mt-5">
+      <p >Loyola Polytechnic(YSRR)</p>
       <p >Designed & developed by S.Madan Mohan Reddy</p>
-      <a href="mailto:forproject0212gmail.com">forproject0212gmail.com</a>
+      <a href="mailto:forproject0212gmail.com">forproject@012gmail.com</a>
     </footer>
   );
 }
